ea869eacbdc62289649ecc4cb69885c398963be2
piyasnhu31 <piyanshukale2005@gmail.com>
2025-09-19 14:06:08 +0530

    hello world

---
+++ dead/build/CMakeFiles/dead.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dead.dir/link.d"
+  "CMakeFiles/dead.dir/src/main.cpp.o"
+  "CMakeFiles/dead.dir/src/main.cpp.o.d"
+  "dead"
+  "dead.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dead.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
