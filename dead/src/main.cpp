171feffd3c911085a49d6f4cd85485b378236e69
piyasnhu31 <piyanshukale2005@gmail.com>
2025-09-20 10:11:35 +0530

    Passing one arg only

--- dead/src/main.cpp
+++ dead/src/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
 int main(int argc,char* argv[]){
+
+    if(argc != 2){
+        std::cerr<<"Incorrect usage, Correct usage is ...."<<std::endl;
+        std::cerr<<"dead <input.dd>"<<std::endl;
+        return EXIT_FAILURE;
+    }
     std::cout << argv[1] << std::endl;
-    return 0;
+    return EXIT_SUCCESS;
 }
